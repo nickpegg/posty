d63134e1ebaa6233ec53d8816912c9732aaac941
Nick Pegg <nick@nickpegg.com>
2018-01-18 20:09:05 -0800

    add feedgen dep

--- setup.py
+++ setup.py
@@ -17,6 +17,7 @@ setup(
     install_requires=[
         'awesome-slugify>=1.6.5,<1.7.0',
         'click>=6.7,<7.0',
+        'feedgen>=0.6.1,<0.7.0',
         'future>=0.16.0,<0.17.0',
         'Jinja2>=2.10,<3.0',
         'Markdown>=2.6.11,<2.7.0',
