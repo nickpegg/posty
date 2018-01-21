3f073a9ac06d37d57925c4e22371015d6d3f8ba5
Nick Pegg <nick@nickpegg.com>
2018-01-20 19:12:36 -0800

    Add Posty1RedirectRenderer
    This is a special renderer that drops redirect files for all Posty1 URLs
    for all posts.

--- posty/renderer/__init__.py
+++ posty/renderer/__init__.py
@@ -2,6 +2,13 @@ from .atom import AtomRenderer
 from .html import HtmlRenderer
 from .json import JsonRenderer
 from .rss import RssRenderer
+from .posty1_redirect import Posty1RedirectRenderer
 
 
-__all__ = [AtomRenderer, HtmlRenderer, JsonRenderer, RssRenderer]
+__all__ = [
+    'AtomRenderer',
+    'HtmlRenderer',
+    'JsonRenderer',
+    'RssRenderer',
+    'Posty1RedirectRenderer'
+]
