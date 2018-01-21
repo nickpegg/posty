fca2dad43073f3dc98ca1cb289576f178a9e539e
Nick Pegg <nick@nickpegg.com>
2018-01-21 14:28:58 -0800

    v2.0 -> v2.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Posty',
-    version='2.0',
+    version='2.0.0',
     author='Nick Pegg',
     author_email='nick@nickpegg.com',
     url='https://github.com/nickpegg/posty',
